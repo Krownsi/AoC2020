aabe8e2ed1892023f2b42f0a414999ca1e2dc501
krownsi <alexander.gruenwald@mail.mcgill.ca>
2020-12-03 01:42:19 -0500

    fixes for readability

--- day3.py
+++ day3.py
@@ -1,28 +1,27 @@
 
 def removeCarriage(line):
-    if "\n" in line:
-        return line.strip('\n')
-    return line
+    return line.strip('\n')
 
 def collisions(incrementor, input_lines):
     f = open("day3.txt", "r")
-    index_right, number_of_trees, count = 0, 0, 1
+    index_right, number_of_trees, count = 0, 0, 0
     for line in f:
-        count += 1
         if count % input_lines == 0:
             line = removeCarriage(line)
-            if len(line) <= index_right: index_right %= len(line)
+            index_right %= len(line)
             if line[index_right] == "#": number_of_trees += 1
             index_right += incrementor
+        count += 1
 
     f.close()
 
     return number_of_trees
 
 def question_2():
+    mult_collisions = 1
     incrementor = [(1, 1), (3, 1), (5, 1), (7, 1), (1, 2)]
     for index in incrementor:
-        mult_collisions = mult_collisions * collisions(index[0], index[1])
+        mult_collisions *= collisions(index[0], index[1])
     print(mult_collisions)
     
 def question_1():
@@ -30,6 +29,7 @@ def question_1():
 
 def main():
     question_1()
+    question_2()
 
 if __name__ == "__main__":
     main()
