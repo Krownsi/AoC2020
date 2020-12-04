26d768aa256fa30f08c10119429739b6f6a8c494
krownsi <alexander.gruenwald@mail.mcgill.ca>
2020-12-04 13:43:38 -0500

    cleaner code

--- day4.py
+++ day4.py
@@ -3,67 +3,62 @@ import re
 def removeCarriage(line):
     return line.strip('\n')
 
-def checkIfValueValid(input):
-    entity = input.split(':')
-    if(len(entity) != 1):
-        value = entity[1]
-        length = len(value)
-        key = entity[0]
-        if key == 'byr' and length == 4 and int(value) >= 1920 and int(value) <= 2002:
+def validHeight(height):
+    if 'cm' in height:
+        cm_value = height.split('cm')
+        if(int(cm_value[0]) >= 150 and int(cm_value[0]) <= 193):
             return 1
-        elif key == 'iyr' and length == 4 and int(value) >= 2010 and int(value) <= 2020: 
-            return 1
-        elif key == 'eyr' and length == 4 and int(value) >= 2020 and int(value) <= 2030: 
-            return 1
-        elif key == 'hgt':
-            if 'cm' in value:
-                cm_value = value.split('cm')
-                if(int(cm_value[0]) >= 150 and int(cm_value[0]) <= 193):
-                    return 1
-            if 'in' in value: 
-                in_value = value.split('in')
-                if(int(in_value[0]) >= 59 and int(in_value[0]) <= 76):
-                    return 1
-        elif key == 'hcl' and re.search('^#([0-9]|[a-f]){6}$', value):
-            return 1
-        elif key in ['ecl', 'amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']:
-            return 1
-        elif key == 'pid' and re.search('^[0-9]{9}$', value):
+    if 'in' in height: 
+        in_value = height.split('in')
+        if(int(in_value[0]) >= 59 and int(in_value[0]) <= 76):
             return 1
     return 0
 
-def checkIfValidQ2(passport):
-    valid_list = map(checkIfValueValid, passport)
-    number_of_checks_passed = sum(list(valid_list))
-    if(number_of_checks_passed == 7):
-        return 1
+def check(passport, part):
+    attributes = ['hcl', 'ecl', 'pid', 'eyr', 'byr', 'iyr', 'hgt']
+    valid = True
+    if(part == 1):
+        valid = all(key in passport for key in attributes)
+        if(valid):
+            return 1
+    elif(part == 2):
+        if not all(key in passport for key in attributes):
+            return 0
+        valid = valid and len(passport['byr']) == 4 and int(passport['byr']) >= 1920 and int(passport['byr']) <= 2002
+        valid = valid and len(passport['iyr']) == 4 and int(passport['iyr']) >= 2010 and int(passport['iyr']) <= 2020
+        valid = valid and len(passport['eyr']) == 4 and int(passport['eyr']) >= 2020 and int(passport['eyr']) <= 2030
+        valid = valid and validHeight(passport['hgt'])
+        valid = valid and re.search('^#([0-9]|[a-f]){6}$', passport['hcl'])
+        valid = valid and passport['ecl'] in ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth']
+        valid = valid and re.search('^[0-9]{9}$', passport['pid'])
+    if valid: return 1
     return 0
 
-def checkIfValidQ1(passport):
-    str_passport = ''.join(passport)
-    if all(key in str_passport for key in ['hcl:', 'ecl:', 'pid:', 'eyr:', 'byr:', 'iyr:', 'hgt:']):
-        return 1
-    return 0
+def convertListToDic(passport):
+    dictionary = {}
+    for entity in passport:
+        dictionary[entity[0]] = entity[1]
+    return dictionary
 
-def run_logic(part):
+def run(part):
     f = open("day4.txt", "r")
     passport = []
     valid_passports = 0
     for line in f:
-        line = removeCarriage(line)
-        if(line == ""):
-            if(part == 1): valid_passports += checkIfValidQ1(passport)
-            else: valid_passports += checkIfValidQ2(passport)
+        line = removeCarriage(line).split()
+        if(line == []):
+            passport = map(lambda x: x.split(':'), passport)
+            valid_passports += check(convertListToDic(list(passport)), part)
             passport = []
-        passport += line.split(' ')
-    if(part == 1): valid_passports += checkIfValidQ1(passport)
-    else: valid_passports += checkIfValidQ2(passport)
+        passport += line
+    passport = map(lambda x: x.split(':'), passport)
+    valid_passports += check(convertListToDic(list(passport)), part)
     f.close()
     return valid_passports
 
 def main():
-    print(run_logic(2))
-    print(run_logic(1))
+    print(run(1))
+    print(run(2))
 
 if __name__ == "__main__":
     main()
