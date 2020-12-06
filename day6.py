2fe332b4beab88184d0b792837b27373bc73e923
krownsi <alexander.gruenwald@mail.mcgill.ca>
2020-12-06 00:26:14 -0500

    day 6 - not clean

---
+++ day6.py
@@ -0,0 +1,33 @@
+def removeCarriage(line):
+    return line.strip('\n')
+
+def countYes(inputValue):
+    listOfChars = {}
+    similar = 0;
+    for people in inputValue:
+        for char in people:
+            if char in listOfChars: listOfChars[char] += 1
+            else: listOfChars[char] = 1
+    for key in listOfChars.keys():
+        if listOfChars[key] == len(inputValue):
+            similar += 1 
+    return similar
+
+def run():
+    f = open("day6.txt", "r")
+    answer = []
+    sum = 0
+    for line in f:
+        line = removeCarriage(line).split()
+        if(line == []):       
+            sum += countYes(answer)
+            answer = []
+        answer += line
+    sum += countYes(answer)
+    print(sum)
+
+def main():
+    run()
+
+if __name__ == "__main__":
+    main()
