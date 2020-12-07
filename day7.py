4caaa8cbf59713543c194c36479c0f3e1c35345b
krownsi <alexander.gruenwald@mail.mcgill.ca>
2020-12-07 11:54:42 -0500

    simplified 1

--- day7.py
+++ day7.py
@@ -3,18 +3,19 @@ def removeCarriage(line):
 
 def run1():
     f = open("day7.txt", "r")  
-    bag_type = ["shiny gold bags", "shiny gold bag"]
+    bag_type = ["shiny gold"]
     previous_check = 0
     while (True):
         for line in f:
             line = removeCarriage(line)
             found = any(bag in line for bag in bag_type)
-            if found and line.split("bags")[0]+'bag' not in bag_type: 
-                bag_type += [line.split("bags")[0] + 'bag', line.split("bags")[0] + 'bags']
+            if found and line.split(" bag")[0] not in bag_type: 
+                bag_type += [line.split(" bags")[0]]
         f.seek(0) 
-        if previous_check != len(bag_type)/2 - 1: previous_check = len(bag_type)/2 - 1
+        if previous_check != len(bag_type) - 1: previous_check = len(bag_type) - 1
         else: break
-    print(len(bag_type)/2 - 1)
+    print(bag_type)
+    print(len(bag_type) - 1)
     f.close()
 
 def count_bags_in_tree(tree, start, number_of_parents):
@@ -34,15 +35,12 @@ def run2():
         if 'no' in line:
             bag_dictionary[line.split(' bags')[0]] = [('0', '0')]
             continue
-        # Add values to the dictionary
         parent_bag = line.split(' bags')[0]
         bag_dictionary[parent_bag] = []
-        # Split each section
         list_of_bags = line.split('contain')[1].split(',')
         for bag in list_of_bags:
             bag_attributes = bag.split(' ')
             bag_dictionary[parent_bag] += [(bag_attributes[2] + ' ' + bag_attributes[3], bag_attributes[1])]
-    
     print(count_bags_in_tree(bag_dictionary, 'shiny gold', 1) - 1)
 
 def main():
